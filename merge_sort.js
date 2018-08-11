7f6892c3221161c47c9ad3edd2c002eabaf8a7e0
apoc64 <sschwedt@gmail.com>
2018-08-11 12:09:00 -0600

    mSort is being called recursively

--- merge_sort.js
+++ merge_sort.js
@@ -5,12 +5,27 @@ class MergeSort {
 
   get sort() {
     var numArray = this.makeArrays()
+    var sorted = this.mSort(numArray)
 
     return numArray
   }
 
   makeArrays() {
-    return this.numbers
+    return this.numbers.map(num => [num]);
+  }
+}
+
+MergeSort.prototype.mSort = function(numArray) {
+  if(numArray.length > 2) {
+    const nums1 = numArray.pop
+    const nums2 = numArray.pop
+    const sortedEnd = this.mSort([nums1, nums2])
+    const sortedBeginning = this.mSort(numArray)
+    return this.mSort([sortedEnd, sortedBeginning])
+  }
+  else
+  {
+    console.log(numArray)
   }
 }
 
